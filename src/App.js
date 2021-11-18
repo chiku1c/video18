4a8c4dba1fca53d6350193e6d465d0a406af413f
chiku1c <chiku00c@gmail.com>
2021-11-18 19:32:15 +0530

    cssstyle

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import Css from './componants/Css';
+import Inline from './Inline';
 // import NameList from './componants/NameList';
-import UserGreeting from './componants/UserGreeting';
+// import UserGreeting from './componants/UserGreeting';
 
 function App() {
   return (
     <div className="App">
       {/* <NameList /> */}
-      <UserGreeting/>
+      {/* <UserGreeting/> */}
+      {/* <Css primary={true} /> */}
+      <Inline />
     </div>
   );
 }
