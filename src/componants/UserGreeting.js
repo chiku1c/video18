0ee89a8a8adca0e27775ed111c961287ef413590
chiku1c <chiku00c@gmail.com>
2021-11-18 17:34:01 +0530

    if/else

--- src/componants/UserGreeting.js
+++ src/componants/UserGreeting.js
@@ -2,13 +2,57 @@ import React, { Component } from 'react'
 
 
 class UserGreeting extends Component {
-    render() {
-        return (
-            <div>
-            <h1>Welcome Sikandar</h1> 
-            </div>
-        )
+    constructor(props){
+        super(props)
+
+        this.state={
+            isLoggedIn:false
+        }
     }
+    render() {
+        return this.state.isLoggedIn && <div>Welocme Sikandar</div> //<componants/>
+
+    }
+
+
+
+
+    //    return this.state.isLoggedIn ?(
+    //     <div>Welcome Sikandar</div>):(<div>Welcome Chauhan</div>)
+
+        // let message
+        // if(this.state.isLoggedIn){
+        //     message=<div>Welocme Sikandar</div>
+        // }else{
+        //     message=<div>Welocme Chauhan</div>
+        // }
+        // return<div>{message}</div>
+    
+
+
+
+
+
+
+        // if(this.state.isLoggedIn){
+        //     return(
+        //         <div>
+        //             welcom sikandar
+        //         </div>
+        //     )
+        //     }else{
+        //         return <div>welcom Guset</div>
+        //     }
+
+            
+        
+        // return (
+        //     <div>
+        //     <h1>Welcome Sikandar</h1> 
+        //     <div>Welcome Guest</div>
+        //     </div>
+        // )
+    
 }
 
 export default UserGreeting
